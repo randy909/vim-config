d944386c8bc848e19300d9a1715ebad3116eb006
root <root@asynchrony.com>
2016-02-11 14:53:55 -0600

    Add install script to compile plugins that need compiling

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ ! -d plugged ]] ; then
+  echo "ERROR: Plugins not installed!"
+  echo "Start vim and run :PlugInstall then try this script again."
+  exit 1
+fi
+
+if [[ ! -e plugged/YouCompleteMe/third_party/ycmd/ycm_core.so ]] ; then
+  echo "Installing YouCompleteMe"
+  python plugged/YouCompleteMe/install.py
+fi
+
+if [[ ! -e plugged/Command-T/ruby/command-t/ext.so ]] ; then
+  echo "Installing Command-T"
+  pushd plugged/Command-T/ruby/command-t
+  ruby extconf.rb
+  make
+  popd
+fi
