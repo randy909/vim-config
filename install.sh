9bda3ad4a0cd3dd41f42a083f3abb22d7c1f581e
Randy Hancock <randy.hancock@asynchrony.com>
2016-06-13 15:43:49 -0500

    Install vim plugins from script

--- install.sh
+++ install.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-if [[ ! -d plugged ]] ; then
-  echo "ERROR: Plugins not installed!"
-  echo "Start vim and run :PlugInstall then try this script again."
-  exit 1
-fi
+echo "Installing/Updating plugins"
+vim +PlugInstall +qall > /dev/null 2>&1
 
 if [[ ! -e plugged/YouCompleteMe/third_party/ycmd/ycm_core.so ]] ; then
-  echo "Installing YouCompleteMe"
+  echo "Compiling YouCompleteMe"
   pushd plugged/YouCompleteMe
   python install.py
   popd
 fi
 
 if [[ ! -e plugged/Command-T/ruby/command-t/ext.so ]] ; then
-  echo "Installing Command-T"
+  echo "Compiling Command-T"
   pushd plugged/Command-T/ruby/command-t
   ruby extconf.rb
   make
